47535df3b9a067044993eb572eeded896c991743
Tomek Rybczyński <tomek.rybka@gmail.com>
2018-03-05 11:24:05 +0100

    Fixing migration

--- db/migrate/20180304072314_create_indexes_for_search.rb
+++ db/migrate/20180304072314_create_indexes_for_search.rb
@@ -1,6 +1,6 @@
 class CreateIndexesForSearch < ActiveRecord::Migration[5.1]
   def up
-    execute "CREATE INDEX questions_gin ON questions USING gin(to_tsvector('english', payload));"
+    execute "CREATE INDEX questions_gin ON questions USING gin(to_tsvector('english'::regconfig, payload));"
   end
 
   def down
