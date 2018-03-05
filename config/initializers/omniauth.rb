048348263f763bd77a76a93bf756f6181fe35e58
Tomek Rybczyński <tomek.rybka@gmail.com>
2018-03-05 11:02:22 +0100

    Added config for omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: ['https://mail.google.com/','contacts','plus.login','plus.me','userinfo.email','userinfo.profile']
+  }
+end
